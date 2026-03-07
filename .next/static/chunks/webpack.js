5fffe553262a19b701ba89e23e3d7d6dafc648bf
lahindu2001 <lahindu2001ishan@gmail.com>
2026-03-07 09:02:05 +0530

    add anothe changes for powerd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "071910f7b0a73620"; }
+/******/ 		__webpack_require__.h = function() { return "4142c7d85a8facf9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
